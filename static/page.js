11a8b21a320b4d902b6b731ec1458e7049be3b18
PoleyMagik <59105820+PoleyMagik@users.noreply.github.com>
2020-01-21 07:53:31 -0500

    Fix

--- static/page.js
+++ static/page.js
@@ -73,7 +73,7 @@ module.exports = function (req, res, url) {
 			params = {
 				flashvars: {
 					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'ut': 60,
-					'autostart': 1, 'clientThemePath': process.env.CLIENT_URL + '/<client_theme>',
+					'autostart': 1,'isWide': 1,  'clientThemePath': process.env.CLIENT_URL + '/<client_theme>',
 				},
 				allowScriptAccess: 'always',
 			};
