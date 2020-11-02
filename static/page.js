1a5f363d492b721afdda0714fbe1472e84fd0010
Windows81 <nastyahmede@gmail.com>
2020-11-02 18:51:47 +0000

    Fixed some syntax errors caused by the cursory work of @BluePeacock.

--- static/page.js
+++ static/page.js
@@ -126,7 +126,7 @@ module.exports = function (req, res, url) {
 					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
 				},
 				allowScriptAccess: "always",
-				allowFullScreen: 'true",
+				allowFullScreen: "true",
 			};
 			break;
 		}
