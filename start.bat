9532f22ee1266c4f93207305dce5a45dd88338a3
narutofan420 <73909524+narutofan420@users.noreply.github.com>
2021-06-08 03:16:00 -0500

    better start.bat (#93)
    * Update start.bat
    
    * Create notinstalled

--- start.bat
+++ start.bat
@@ -1,2 +1,32 @@
-npm install
+:: Important stuff
+@echo off && cls
+title GoAnimate Wrapper
+
+::::::::::::::::::::
+:: Initialization ::
+::::::::::::::::::::
+
+:: Terminate existing node.js apps
+TASKKILL /IM node.exe /F 2>nul
+cls
+
+:::::::::::::::::::::::::::::
+:: Start GoAnimate Wrapper ::
+:::::::::::::::::::::::::::::
+
+:: Check for installation
+if exist notinstalled (
+	echo GoAnimate Wrapper is not installed! Installing...
+	call npm install
+	ren "notinstalled" "installed"
+	cls
+	goto start
+) else (
+	goto start
+)
+
+:: Run npm start
+:start
+echo GoAnimate Wrapper is now starting...
+echo Please navigate to http://localhost on your browser.
 npm start
